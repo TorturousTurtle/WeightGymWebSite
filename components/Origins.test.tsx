8651f9733123b250c57cd56c2e808439c8b3e3f2
TorturousTurtle <bverrill88@gmail.com>
2023-10-10 11:18:22 -0400

    bug fix

--- components/Origins.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import Origins from "./Origins";
-
-// test("renders Weight Gym! as text", () => {
-//   render(<Origins />);
-//   const weightGymElement = screen.getByText(/Weight Gym!/);
-//   expect(weightGymElement).toBeInTheDocument();
-// });
