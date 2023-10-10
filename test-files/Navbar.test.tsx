38f830c968d6b1a7ac26d1d028621c2935b0b90b
TorturousTurtle <bverrill88@gmail.com>
2023-10-10 11:20:45 -0400

    bug fix

---
+++ test-files/Navbar.test.tsx
@@ -0,0 +1,21 @@
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import "@testing-library/jest-dom";
+// import Navbar from "./Navbar";
+
+// describe("Navbar component", () => {
+//   test("renders Weight Gym! as text", () => {
+//     render(<Navbar />);
+//     const weightGymElement = screen.getByText(/Weight/);
+//     expect(weightGymElement).toBeInTheDocument();
+//   });
+
+//   test("Login clicked", () => {
+//     render(<Navbar />);
+//     const buttonElement = screen.getByRole("button");
+//     userEvent.click(buttonElement);
+
+//     const outputElement = screen.getByText("Sign In", { exact: false });
+//     expect(outputElement).toBeInTheDocument();
+//   });
+// });
---
+++ test-files/login.test.tsx
@@ -0,0 +1,12 @@
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import "@testing-library/jest-dom";
+// import LoginScreen from "./login";
+
+// describe("LoginScreen component", () => {
+//   test("renders Weight Gym! as text", () => {
+//     render(<LoginScreen />);
+//     const weightGymElement = screen.getByText(/Weight/);
+//     expect(weightGymElement).toBeInTheDocument();
+//   });
+// });
